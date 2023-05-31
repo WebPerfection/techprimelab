894c9671a88498ead2bca01f1e0a500f9bc8d841
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-05-31 21:57:24 +0530

    login

--- Frontend/techprimelab/src/Login/Login.js
+++ Frontend/techprimelab/src/Login/Login.js
@@ -7,20 +7,33 @@ import { useNavigate } from 'react-router-dom';
 import axios from "axios"
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const navigate=useNavigate()
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
-    const payload={
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const payload = {
         email,
         password
     }
 
-    const onHendell=(e)=>{
-        e.preventDefault()
+    const onHendell = (e) => {
+        if (email === '') {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
         
-        axios.post("http://localhost:8080/login",payload)
-        .then((res)=>navigate("/dashboard"))
-        .catch(err=>console.log(err))
+        if (password === '') {
+            setPasswordError(true);
+        } else {
+            setPasswordError(false);
+        }
+        e.preventDefault()
+
+        axios.post("http://localhost:8080/login", payload)
+            .then((res) => navigate("/dashboard"))
+            .catch(err => console.log(err))
     }
     const toggleShowPassword = () => {
         setShowPassword(!showPassword);
@@ -28,49 +41,58 @@ const Login = () => {
 
     return (
         <div className="login-container">
-            <div className="container">
-                <div className="logo-container">
-                    <img src={logo} alt="Logo" />
-                    <h4>Online Project Management</h4>
-                </div>
-                <div className="form-container">
-                    <form>
-                        <h3>Login to get start</h3>
-                        <span>Email</span>
-                        <div className="Username-input-container">
-
-                            <input type="text" placeholder="Username"  onChange={e=>setEmail(e.target.value)}/>
-                        </div>
-                        <span>Password</span>
-
-                        <div className="password-input-container">
-                            <input
-                                type={showPassword ? 'text' : 'password'}
-                                placeholder="Password"
-                                onChange={e=>setPassword(e.target.value)}
-                            />
-                            <span
-                                className={`password-toggle ${showPassword ? 'visible' : ''}`}
-                                onClick={toggleShowPassword}
-                            >
-                                {showPassword ? (
-                                    <FontAwesomeIcon icon={faEyeSlash} />
-                                ) : (
-                                    <FontAwesomeIcon icon={faEye} />
-                                )}
-                            </span>
-                        </div>
-                        <div className="forgot-password">
-                            <a href="#" className="red-link">
-                                Forgot Password?
-                            </a>
-                        </div>
-                        <button type="submit" onClick={onHendell}>Login</button>
-                    </form>
-                </div>
+        <div className="container">
+            <div className="logo-container">
+                <img src={logo} alt="Logo" />
+                <h4>Online Project Management</h4>
             </div>
-
+            <div className="form-container">
+                <form onSubmit={onHendell}>
+                    <h3>Login to get started</h3>
+                    <span  style={{color:emailError ? "red":''}}>Email</span>
+                    <div className="Username-input-container">
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className={emailError ? 'error' : ''}
+                            
+                        />
+                    </div>
+                    {emailError && <span className="error-text">Email is required</span>}
+                    <span  style={{color:passwordError ? "red":''}}>Password</span>
+                    <div className="password-input-container">
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className={passwordError ? 'error' : ''}
+                            
+                        />
+                        <span
+                            className={`password-toggle ${showPassword ? 'visible' : ''}`}
+                            onClick={toggleShowPassword}
+                        >
+                            {showPassword ? (
+                                <FontAwesomeIcon icon={faEyeSlash} />
+                            ) : (
+                                <FontAwesomeIcon icon={faEye} />
+                            )}
+                        </span>
+                    </div>
+                    {passwordError && <span className="error-text">Password is required</span>}
+                    <div className="forgot-password">
+                        <a href="#" className="red-link">
+                            Forgot Password?
+                        </a>
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
+    </div>
     );
 };
 
