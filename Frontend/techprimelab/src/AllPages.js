9a5a290e8b99b27099b8d07fba4a9921d64db06b
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-06-03 19:00:14 +0530

    login

--- Frontend/techprimelab/src/AllPages.js
+++ Frontend/techprimelab/src/AllPages.js
@@ -4,11 +4,13 @@ import BarChart from './Dashboard/Chat'
 import Dashboard from './Dashboard/Dashboard'
 import Login from './Login/Login'
 import Sidebar from './Navbar/Sidebar/Sidebar'
+import DashboardBody from './Navbar/Secondbar/DashboardBody'
 export default function AllPages() {
   return (
     <Routes>
         <Route path='/' element={<Login/>} ></Route>
         <Route path='/dashboard' element={<Sidebar/>} ></Route>
+        <Route path='/DashboardBody' element={<DashboardBody/>} ></Route>
     </Routes>
   )
 }
